9490eed0e77b34c231a98b0d4338d3e4e994c9e2
slmnv5 <slmnv5@gmail.com>
2022-12-10 17:38:06 -0500

    1

--- src/FrameBuff.hpp
+++ src/FrameBuff.hpp
@@ -125,7 +125,21 @@ public:
         font = &f;
     }
 
-    void put_char(int x, int y, unsigned char font_chr, uint colidx)
+    void put_char(int x, int y, int c, int colidx)
+    {
+        int i, j, bits;
+        for (i = 0; i < font->height; i++)
+        {
+            bits = font->data[font->height * c + i];
+            for (j = 0; j < font->width; j++, bits <<= 1)
+                if (bits & 0x80)
+                {
+                    put_pixel_16bpp(x + j, y + i, 255, 255, 255);
+                }
+        }
+    }
+
+    void put_char111(int x, int y, unsigned char font_chr, uint colidx)
     {
         uint char_w = font->width / 8;
         uint char_sz = font->height * char_w;
